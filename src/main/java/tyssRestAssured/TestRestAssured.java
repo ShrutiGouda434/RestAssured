f231b65ee2874fb1cf14814c12a7f6859c30ed66
qspidersseleniumoar <qspidersseleniumoar@gmail.com>
2018-09-03 15:18:07 +0530

    first restassured commit

---
+++ src/main/java/tyssRestAssured/TestRestAssured.java
@@ -0,0 +1,19 @@
+package tyssRestAssured;
+
+
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class TestRestAssured {
+	@Test
+public void getApiTest(){
+Response resp = RestAssured.get("http://localhost:3000/posts");
+int statusCode = resp.getStatusCode();
+System.out.println(statusCode);
+System.out.println( resp.getContentType());
+
+}
+}
